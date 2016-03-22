6e127c9d0a1f3d7fe3bfd7e5fe77508b0093c75e
Wayne Keenan <wayne.keenan@gmail.com>
2016-03-22 15:00:12 +0000

    added info about self-sighe SSL cert. moved cert genration to separete script and added option for non prompting

---
+++ createSelfSignedCertiicate.sh
@@ -0,0 +1,3 @@
+openssl req -new -x509 -keyout server.pem -out server.pem -days 365 -nodes -subj "/C=GB/ST=Cambridge/L=Cambridge/O=TheBubbleWorks/OU=Playground/CN=localhost"
+
+
--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 #!/usr/bin/env python
-
-import BaseHTTPServer, SimpleHTTPServer
+import BaseHTTPServer
+import SimpleHTTPServer
 import ssl
 
-#To gerneate a new server certificate:
-#   openssl req -new -x509 -keyout server.pem -out server.pem -days 365 -nodes
-
+print 
+print "Open a browser at: https://localhost:9443"
+print
+print "*** You will get an warning about the site being unsafe, for example:"
+print
+print "\tChrome: `Your connection is not private'"
+print "\tSafari: `Can't verify the identiry of the website'"
+print
+print "This is expected and is ok, just click:"
+print
+print "\tChrome: `Proceed to localhost (unsafe)`"
+print "\tSafari: `Continue`"
+print
 httpd = BaseHTTPServer.HTTPServer(('0.0.0.0', 9443), SimpleHTTPServer.SimpleHTTPRequestHandler)
 httpd.socket = ssl.wrap_socket (httpd.socket, certfile='server.pem', server_side=True)
 httpd.serve_forever()
